c762c87c42d3a524f7881f98ebd27b880227c4af
Ferkze <ferkemartins@gmail.com>
2020-05-21 12:08:42 -0300

    Added login_test flux functions

--- clear/login_test.go
+++ clear/login_test.go
@@ -4,28 +4,34 @@ import (
 	"github.com/cucumber/godog"
 	messages "github.com/cucumber/messages-go/v10"
 	"github.com/ferkze/personal-finance-crawlers/clear/support"
+	"github.com/ferkze/personal-finance-crawlers/clear/types"
 	"github.com/tebeka/selenium"
 )
 
 var Driver selenium.WebDriver
 
 func imAccessingTheLoginPage() error {
-	
-	return godog.ErrPending
+	return Driver.Get("https://www.clear.com.br/pit/signin?controller=SignIn")
 }
 
 func iFillTheLoginForm() error {
-	return godog.ErrPending
+	acc := types.Account{
+}
+	return login(&Driver, acc)
 }
 
-func iGetRedirectedToThePitSelection() error {
-	return godog.ErrPending
+func iGetRedirectedToThePitSelection() (err error) {
+	return selectPit(&Driver)
+}
+func iCanAccessTheOrdersPage() error {
+	return navigateToOrders(&Driver)
 }
 
 func FeatureContext(s *godog.Suite) {
 	s.Step(`^I\'m accessing the login page$`, imAccessingTheLoginPage)
 	s.Step(`^I fill the login form$`, iFillTheLoginForm)
 	s.Step(`^I get redirected to the pit selection$`, iGetRedirectedToThePitSelection)
+	s.Step(`^I can access the orders page$`, iCanAccessTheOrdersPage)
 
 	s.BeforeScenario(func(*messages.Pickle) {
 		Driver = support.WBInit()
