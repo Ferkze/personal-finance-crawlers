f1836da23e47e284c41be410ed0eb4f0dfba79cb
Ferkze <ferkemartins@gmail.com>
2020-05-25 19:36:45 -0300

    Added typos for orderType and assetType + godoc

--- clear/types/types.go
+++ clear/types/types.go
@@ -2,12 +2,14 @@ package types
 
 import "time"
 
+// Account credenciais de login para conta da Clear
 type Account struct {
 	CPF string
 	Password string
 	DateOfBirth string
 }
 
+// Execution representa a execução de uma ordem, podendo existir várias execuções para uma ordem
 type Execution struct {
 	Asset string
 	AssetType string
@@ -16,3 +18,9 @@ type Execution struct {
 	Price float64
 	Datetime time.Time	
 }
+
+// OrderType representa o tipo de ordem de Compra ou de Venda
+type OrderType string
+
+// AssetType representa o tipo de ativo negociado, sendo DolarFuturo, IndiceFuturo ou Acoes
+type AssetType string
