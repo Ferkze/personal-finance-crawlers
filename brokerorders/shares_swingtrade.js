4d0d43a70a0877edff19ce41f22254b0589f3468
Ferkze <ferkemartins@gmail.com>
2020-06-27 18:34:37 -0300

    added swingtrade exception cases for results calculing

--- brokerorders/shares_swingtrade.js
+++ brokerorders/shares_swingtrade.js
@@ -5,7 +5,7 @@ console.info('Running shares script')
 let orders = JSON.parse(fs.readFileSync('orders.json'));
 
 const shares = orders.filter(o => o.assetType == 'Ações')
-	.sort((a,b) => new Date(b.date) - new Date(a.date))
+	.sort((a,b) => new Date(a.date) - new Date(b.date))
 	.map(o => {
 		o.date = o.date.substring(0, 10)
 		return o
@@ -69,7 +69,6 @@ function calculateSwingtradesResult(positions = {'': {totalVolume: 0, totalBuyin
 		costs: 0,
 		irrf: 0
 	}
-	// let stcount = Object.values(positions).reduce((acc, cur) => cur.operation == 'swingtrade' ? ++acc : acc, 0)
 
 	for (const positionKey of Object.keys(positions)) {
 		const position = positions[positionKey]
@@ -83,57 +82,23 @@ function calculateSwingtradesResult(positions = {'': {totalVolume: 0, totalBuyin
 				quant: 0
 			}
 		}
+		handleStockDivision(positionKey, position)
 		if (position.operation == 'swingtrade') {
-			if (position.buyQnt > 0) { // compra
-				if (st.quant >= 0 && st.total >= 0) {// comprado
-					st.total += position.totalBuyingVolume
-					st.quant += position.buyQnt
-				} else {// vendido
-					let avgPriceShort = st.total/st.quant
-					let avgPriceLong = position.totalBuyingVolume/position.buyQnt
-					if (position.buyQnt <= st.quant) {
-						results.total += (avgPriceShort - avgPriceLong)*position.buyQnt
-						st.quant += position.buyQnt
-						st.total += position.totalBuyingVolume
-					} else {
-						console.log('tratar 1')
-					}
-				}
-				results.volume += position.totalBuyingVolume
-			} else if(position.sellingQnt > 0) { // venda
-				if (st.quant >= 0 && st.total >= 0) {// comprado
-					let avgPriceShort = position.totalSellingVolume/position.sellingQnt
-					let avgPriceLong = st.total/st.quant
-					if (position.sellingQnt <= st.quant) {
-						results.total += (avgPriceShort - avgPriceLong)*position.sellingQnt
-						st.quant -= position.sellingQnt
-						st.total -= position.totalSellingVolume
-					} else {
-						console.log('tratar 2')
-					}
-				} else {// vendido
-					st.total -= position.totalSellingVolume
-					st.quant += position.sellingQnt
-				}
-				results.volume += position.totalSellingVolume
-				results.sold += position.totalSellingVolume
-			}
+			consolidateResult(results, st, position, positionKey)
 		} else if (position.operation == 'mixed') {
-			console.log('tratar 3')
-			// if (position.buyQnt > 0 && position.sellingQnt > 0) {
-			// 	const excedent = position.buyQnt - position.sellingQnt
-			// 	const buyVolDT = position.totalBuyingVolume - ((position.totalBuyingVolume/position.buyQnt)*excedent)
-			// 	const res = position.totalSellingVolume - buyVolDT
-			// 	results.total += res
-			// } else if (position.sellingQnt > position.buyQnt && position.buyQnt > 0) {
-			// 	const excedent = position.sellingQnt - position.buyQnt
-			// 	const shortVolDT = position.totalSellingVolume - ((position.totalSellingVolume/position.sellingQnt)*excedent)
-			// 	const res = shortVolDT - position.totalBuyingVolume
-			// 	results.total += res
-			// } else {
-			// 	const res = position.totalSellingVolume - position.totalBuyingVolume
-			// 	results.total += res
-			// }
+			let pos
+			if (position.buyQnt > position.sellingQnt) {
+				pos = {
+					totalBuyingVolume: position.totalBuyingVolume - position.totalSellingVolume,
+					buyQnt: position.buyQnt - position.sellingQnt
+				}
+			} else {
+				pos = {
+					totalSellingVolume: position.totalSellingVolume - position.totalBuyingVolume,
+					sellingQnt: position.sellingQnt - position.buyQnt
+				}
+			}
+			consolidateResult(results, st, pos, positionKey)
 		}
 		swingtrades[positionKey] = st
 		if (swingtrades[positionKey].quant == 0) {
@@ -176,10 +141,61 @@ function groupResultsByPeriod(results, period='month') {
 	}, {})
 }
 
+function consolidateResult(results, swingtrade, position, positionKey) {
+	if (position.buyQnt > 0) { // compra
+		if (swingtrade.quant >= 0 && swingtrade.total >= 0) {// comprado
+			swingtrade.total += position.totalBuyingVolume
+			swingtrade.quant += position.buyQnt
+		} else {// vendido
+			let avgPriceShort = swingtrade.total/swingtrade.quant
+			let avgPriceLong = position.totalBuyingVolume/position.buyQnt
+			if (position.buyQnt <= swingtrade.quant) {
+				results.total += (avgPriceShort - avgPriceLong)*position.buyQnt
+				swingtrade.quant += position.buyQnt
+				swingtrade.total += position.totalBuyingVolume
+			} else {
+				console.log('tratar 1') // comprar posicao maior do que esta vendido
+			}
+		}
+		results.volume += position.totalBuyingVolume
+	} else if(position.sellingQnt > 0) { // venda
+		if (swingtrade.quant >= 0 && swingtrade.total >= 0) {// comprado
+			let avgPriceShort = position.totalSellingVolume/position.sellingQnt
+			let avgPriceLong = swingtrade.total/swingtrade.quant
+			if (position.sellingQnt <= swingtrade.quant) {
+				results.total += (avgPriceShort - avgPriceLong)*position.sellingQnt
+				swingtrade.quant -= position.sellingQnt
+				swingtrade.total -= position.totalSellingVolume
+			} else {
+				results.total += (avgPriceShort - avgPriceLong)*swingtrade.quant
+				const quantLeft = position.sellingQnt - swingtrade.quant
+				swingtrade.quant = quantLeft
+				swingtrade.total = quantLeft * avgPriceShort
+			}
+		} else {// vendido
+			swingtrade.total -= position.totalSellingVolume
+			swingtrade.quant += position.sellingQnt
+		}
+		results.volume += position.totalSellingVolume
+		results.sold += position.totalSellingVolume
+	}
+}
+
+function handleStockDivision(key, position) {
+	if (key == 'TRAN PAULIST') {
+		positionDate = new Date(position.date).getTime()
+		stockDivisionDate = new Date('2019-04-04').getTime()
+		if (positionDate <= stockDivisionDate) {
+			position.buyQnt *= 4
+			position.sellingQnt *= 4
+		}
+	}
+}
+
 function printResults(results) {
 	Object.keys(results).forEach(k => {
 		const r = results[k]
 		console.log(`Período ${k}`)
-		console.log(`Resultado Líquido: R$ ${(r.total - r.costs).toFixed(2).padStart(8, ' ')} ; Custos: R$ ${(r.costs).toFixed(2).padStart(8, ' ')} ; IRRF: R$ ${r.irrf.toFixed(2).padStart(8, ' ')}`)
+		console.log(`Resultado Líquido: R$ ${(r.total - r.costs - r.irrf).toFixed(2).padStart(8, ' ')} ; Custos: R$ ${(r.costs).toFixed(2).padStart(8, ' ')} ; IRRF: R$ ${r.irrf.toFixed(2).padStart(8, ' ')}`)
 	})
 }
